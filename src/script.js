6f10977ff13314c06e50076505e0195640b73b22
Soraya <icuestiona@gmail.com>
2022-06-10 12:16:00 +0200

    Changed hours-minutes js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,13 @@
 let now = new Date();
 let h3 = document.querySelector(`h3`);
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let days = [
   `Sunday`,
   `Monday`,
